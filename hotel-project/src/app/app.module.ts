e796b3178c60d95bd1424da71fb863e27b5e611b
marcincwiek <marcincwiek77@gmail.com>
2023-10-25 23:44:15 +0300

    Uploading Base of the Project to github
    First Angular Project

---
+++ hotel-project/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+// import { CustomersComponent } from './customers/customers.component';
+// import { CustomersBrowserComponent } from './customers-browser/customers-browser.component';
+// import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { CustomersService } from './customers/customers.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    // CustomersComponent,
+    // CustomersBrowserComponent,
+    // NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [
+    // CustomersService,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
