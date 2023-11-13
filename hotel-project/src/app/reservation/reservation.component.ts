51d5f5a35b1331bbb4bb7156b10fe7134b089f86
marcincwiek <marcincwiek77@gmail.com>
2023-11-13 13:19:15 +0300

    Week 7.1 Update
    Added a new filter to data displayed in the table, to only show reservations from the specified time.

--- hotel-project/src/app/reservation/reservation.component.ts
+++ hotel-project/src/app/reservation/reservation.component.ts
@@ -14,7 +14,17 @@ export class ReservationComponent {
     this.service.getReservation().then(
       (data) => {
         this.reservations = data;
+        this.filterReservationsForLastWeek();
       }
     )
+  }
+  filterReservationsForLastWeek() {
+    const today = new Date();
+    const lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+
+    this.reservations = this.reservations.filter((reservation: any) => {
+      const updateDate = new Date(reservation.dateUpdated);
+      return updateDate >= lastWeek;
+    });
   }
 }
